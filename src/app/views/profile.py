04f9aa5c0dc2a1ccc3dfb999d7d528eda1b07070
Dinh_Ngoc <bvngoc2000@gmail.com>
2020-12-19 18:31:18 +0700

    fix profile exist and user exist

--- src/app/views/profile.py
+++ src/app/views/profile.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, render_template, abort
 from flask import request, session
+from sqlalchemy import or_
 from src.app import db
 from src.app.models import Users
 
@@ -12,20 +13,35 @@ def profile():
     login = session.get("logged_in")
     if login:
         username = session.get("username")
-        user = db.session.query(Users).filter(Users.username==username).first()
+        user_id = session.get("user_id")
+        update_user = db.session.query(Users).filter(Users.id==user_id).first()
 
         if request.method == "POST":
             f_email = request.form["email"]
             f_username = request.form["username"]
             f_name = request.form["name"]
             f_phone_number = request.form["phone"]
+
+            if f_name == 'None':
+                f_name = None
+            if f_phone_number == 'None':
+                f_phone_number = None
             
-            update_user.email = f_email
-            update_user.username = f_username
-            update_user.name = f_name
-            update_user.phone_number = f_phone_number
-            db.session.commit()
-        return render_template("profile/profile.html",login=login, user=username, uuser=user)
+            email_exist = db.session.query(Users).filter(Users.email==f_email).first()
+            username_exist = db.session.query(Users).filter(Users.username==f_username).first()
+
+            if update_user is not (email_exist and username_exist):
+                return render_template("profile/profile.html",login=login, user=username, uuser=update_user, user_exist=True)
+            else:           
+                update_user.email = f_email
+                update_user.username = f_username
+                update_user.name = f_name
+                update_user.phone_number = f_phone_number
+                db.session.commit()
+
+                return render_template("profile/profile.html",login=login, user=username, uuser=update_user)
+
+        return render_template("profile/profile.html",login=login, user=username, uuser=update_user)
     else:
         return abort(401)
 
