76fa50c6c0bbc456dda63fa3423750c46377a0c0
Dinh_Ngoc <bvngoc2000@gmail.com>
2020-12-22 09:45:06 +0700

    fix profile

--- src/app/views/profile.py
+++ src/app/views/profile.py
@@ -15,6 +15,8 @@ def profile():
         username = session.get("username")
         user_id = session.get("user_id")
         update_user = db.session.query(Users).filter(Users.id==user_id).first()
+        if not update_user:
+            return abort(404)
 
         if request.method == "POST":
             f_email = request.form["email"]
@@ -26,19 +28,26 @@ def profile():
                 f_name = None
             if f_phone_number == 'None':
                 f_phone_number = None
-            
-            email_exist = db.session.query(Users.id).filter(Users.email==f_email).first()
-            username_exist = db.session.query(Users.id).filter(Users.username==f_username).first()
 
-            if update_user is not (email_exist and username_exist):
+            if update_user.username == f_username and update_user.email == f_email:
+                update_user.name = f_name
+                update_user.phone_number = f_phone_number
+                db.session.commit()
+                return render_template("profile/profile.html",login=login, user=username, uuser=update_user)
+            query = []
+            if update_user.email != f_email:
+                query.append(Users.email == f_email)
+            if update_user.username != f_username:
+                query.append(Users.username == f_username)
+            user_exist = db.session.query(Users.id).filter(or_(*query)).first()
+            if update_user and user_exist:
                 return render_template("profile/profile.html",login=login, user=username, uuser=update_user, user_exist=True)
-            else:           
+            else:
                 update_user.email = f_email
                 update_user.username = f_username
                 update_user.name = f_name
                 update_user.phone_number = f_phone_number
                 db.session.commit()
-
                 return render_template("profile/profile.html",login=login, user=username, uuser=update_user)
 
         return render_template("profile/profile.html",login=login, user=username, uuser=update_user)
